e26418df05d4f4756b1fb0ae7860de376f1443fe
Pablo George <pablogeokar@hotmail.com>
2016-11-27 14:49:14 -0300

    ETAPA 01

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-Vue.filter('doneLabel', function(value){
-    if(value == 0){
+Vue.filter('doneLabel', function (value) {
+    if (value == 0) {
         return "Não Paga"
     } else {
         return "Paga"
     }
+});
+Vue.filter('statusLabel', function (value) {
+    if (value === false) {
+        return "Nenhuma conta cadastrada"
+    }
+    
+    if (!value){
+        return "Nenhuma conta a pagar"
+    } else {
+        return "Existem " + value + " conta(s) a ser(em) paga(s)"
+    }
+    
+    
 });
 var app = new Vue({
     el: "#appVue",
@@ -15,57 +28,79 @@ var app = new Vue({
             {id: 1, name: "Criar Conta"}
         ],
         names: [
-           'Conta de Luz',
-           'Conta de Água',
-           'Conta de Telefone',
-           'Supermercado',
-           'Conta de Água',
-           'Empréstimo',
-           'Gasolina'
+            'Conta de Luz',
+            'Conta de Água',
+            'Conta de Telefone',
+            'Supermercado',
+            'Conta de Água',
+            'Empréstimo',
+            'Gasolina'
         ],
         formType: 'insert',
         bill: {
-          date_due: ""  ,
-          name: "",
-          value: 0,
-          done: 0
+            date_due: "",
+            name: "",
+            value: 0,
+            done: 0
         },
-        bills:[
-            {date_due: "25/11/2016", name: "Conta de Luz", value: 25.99, done:1}, 
-            {date_due: "25/11/2016", name: "Conta de Água", value: 220.99, done:0},
-            {date_due: "25/11/2016", name: "Gasolina", value: 425.99, done:0},
-            {date_due: "25/11/2016", name: "Gasolina", value: 33.49, done:0},
-            {date_due: "25/11/2016", name: "Empréstimo", value: 28.99, done:0},
-            {date_due: "25/11/2016", name: "Conta de Água", value: 125.99, done:0},
-            {date_due: "25/11/2016", name: "Conta de Telefone", value: 125.99, done:0}
+        bills: [
+            {date_due: "25/11/2016", name: "Conta de Luz", value: 25.99, done: 1},
+            {date_due: "25/11/2016", name: "Conta de Água", value: 220.99, done: 0},
+            {date_due: "25/11/2016", name: "Gasolina", value: 425.99, done: 0},
+            {date_due: "25/11/2016", name: "Gasolina", value: 33.49, done: 0},
+            {date_due: "25/11/2016", name: "Empréstimo", value: 28.99, done: 0},
+            {date_due: "25/11/2016", name: "Conta de Água", value: 125.99, done: 0},
+            {date_due: "25/11/2016", name: "Conta de Telefone", value: 125.99, done: 0}
         ]
     },
+    computed: {
+        status: function(){
+            if(!this.bills.length){
+                return false;
+            }
+            var count = 0;
+            for (var i in this.bills) {
+                if(!this.bills[i].done){
+                    count++;
+                }
+            }
+            return count;
+        } 
+    },
     methods: {
-        showView: function(id){
+        showView: function (id) {
             this.activedView = id;
-            if(id == 1){
-              this.formType = 'insert'  ;
+            if (id == 1) {
+                this.formType = 'insert';
             }
         },
-        submit: function(){
-            if(this.formType == 'insert'){
+        submit: function () {
+            if (this.formType == 'insert') {
                 this.bills.push(this.bill);
-            };
-            
-          this.bill = {
-          date_due: ""  ,
-          name: "",
-          value: 0,
-          done: 0
-        }
-            
-         this.activedView = 0;
+            }
+            ;
+
+            this.bill = {
+                date_due: "",
+                name: "",
+                value: 0,
+                done: 0
+            }
+
+            this.activedView = 0;
 
         },
-        loadBill: function(bill){
+        loadBill: function (bill) {
             this.bill = bill;
             this.activedView = 1;
             this.formType = 'update';
+        },
+        deleteBill: function (o) {
+            if (confirm('Deseja Excluir esta conta?')) {
+                this.bills.$remove(o);
+            }
+
+
         }
     }
 });
